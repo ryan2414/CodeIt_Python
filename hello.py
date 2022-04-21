b909747a54a9f9f04092537073bdbace41faaed8
ryan2414 <75060346+ryan2414@users.noreply.github.com>
2022-04-21 20:28:53 +0900

    Start Python

---
+++ hello.py
@@ -0,0 +1,8 @@
+burger_price = 4900
+fries_price = 1490
+dring_price = 1280
+
+print(burger_price)
+print(burger_price * 2)
+print(burger_price + fries_price)
+print(burger_price * 3 + fries_price * 2 + dring_price * 5)
