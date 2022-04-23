87b5f0aa998e6e6e3c05b3d03813d4a87b034917
ryan2414 <75060346+ryan2414@users.noreply.github.com>
2022-04-23 12:41:23 +0900

    Study Python

--- study_dictionary.py
+++ study_dictionary.py
@@ -9,3 +9,22 @@ my_dictionary = {
 print(my_dictionary[3])
 my_dictionary[9] = 81
 print(my_dictionary)
+
+# value 값 보기
+print(my_dictionary.values())
+print(5 in my_dictionary.values())
+
+for value in my_dictionary.values():
+    print(value)
+
+# key 값 보기
+print(my_dictionary.keys())
+
+for key in my_dictionary.keys():
+    value = my_dictionary[key]
+    print(key, value)
+
+print()
+# key와 value 한번에 가져오기
+for key, value in my_dictionary.items():
+    print(key, value)
