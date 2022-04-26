a4c8dc997a913a4f78f8e6bd36fda9daf898f96a
ryan2414 <75060346+ryan2414@users.noreply.github.com>
2022-04-26 23:00:05 +0900

    Study Module

---
+++ Study_Module/run.py
@@ -0,0 +1,4 @@
+import  area as ar
+
+print(ar.circle(2))
+print(ar.square(3))
--- Study_Python_basic/exercise_chicken.py
+++ Study_Python_basic/exercise_chicken.py
@@ -1,7 +1,7 @@
 total = 0
 day = 31
 
-with open("data/chicken.txt", "r") as f:
+with open("../data/chicken.txt", "r") as f:
     for line in f:
         split_line = line.strip().split(': ')
         total += int(split_line[1])
